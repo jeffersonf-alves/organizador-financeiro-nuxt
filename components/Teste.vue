05e7444dea9c7d81ddb9a0718023fa56142aa570
Jefferson Alves <jefff9910@outlook.com>
2024-06-23 13:13:43 -0300

    Adicionando tailwind

---
+++ components/Teste.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Testando meu compoenent</h1>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+     <h1 class="text-3xl font-bold underline bg-sky-500">
+        Hello world!
+  </h1>
+</template>
